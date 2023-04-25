7c4c351113a238517d4a908173e63194b36a448f
Kinjen0 <118626914+Kinjen0@users.noreply.github.com>
2023-04-25 11:44:01 -0400

    added map logic

--- Hero Game c++/maps.cpp
+++ Hero Game c++/maps.cpp
@@ -1,7 +1,56 @@
+#include <SFML/Graphics.hpp>
+
+
 class  maps
 {
+private:
+	sf::Texture mapTexture;
+	sf::Sprite baseGround;
+	sf::Sprite wallTexture;
+	int map[10][10] = {
+	{ 1,1,1,1,1,1,1,1,1,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,2,2,0,0,0,1 },
+	{ 1,0,0,0,2,2,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,1,1,1,1,1,1,1,1,1 }
+	};
 	
+public:
+	maps()
+	{
+		mapTexture.loadFromFile("Graphics/Grass.png");
+		baseGround.setTexture(mapTexture);
+		wallTexture.setTexture(mapTexture);
+		baseGround.setTextureRect(sf::IntRect(32,32, 0, 0));
+		wallTexture.setTextureRect(sf::IntRect(14, 14, 49, 33));
+	}
+	
+	~maps();
 
+	void drawMap()
+	{
+		//draw the map onto the screen
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; i < 10; i++)
+			{
+				if (map[i][j] == 1)
+				{
+					baseGround.setPosition(i * 32, j * 32);
+				}
+				else if (map[i][j] == 2)
+				{
+					wallTexture.setPosition(i * 32, j * 32);
+					
+				}
+			}
+		}
+	}
 
 
 
